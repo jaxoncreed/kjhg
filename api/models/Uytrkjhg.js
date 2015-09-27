faedc6a4d5c7daf375418168b542610a83161839
Jackson Morgan <jaxoncreed@gmail.com>
2015-09-27 12:54:07 -0400

    Initial Commit from Restful.net

---
+++ api/models/Uytrkjhg.js
@@ -0,0 +1,20 @@
+/**
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+attributes: {
+    "jhg": {
+        "key": "jhg",
+        "autoIncrement": false,
+        "unique": false,
+        "primaryKey": false,
+        "required": false,
+        "collection": false,
+        "model": "okjhghjhg"
+    }
+}
+
+};
+
